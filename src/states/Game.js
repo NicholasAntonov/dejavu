c6ece689ff49217341d076125c6c3d3214c46ff3
nicholas antonov <nicholasantonov@gmail.com>
2017-04-11 23:52:21 -0400

    Add car

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,34 +1,47 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
 import Mushroom from '../sprites/Mushroom'
+import Car from '../sprites/Car'
 
 export default class extends Phaser.State {
-  init () {}
-  preload () {}
+    init () {}
+    preload () {}
 
-  create () {
-    const bannerText = 'Phaser + ES6 + Webpack'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
-    banner.font = 'Bangers'
-    banner.padding.set(10, 16)
-    banner.fontSize = 40
-    banner.fill = '#77BFA3'
-    banner.smoothed = false
-    banner.anchor.setTo(0.5)
+    create () {
+        const bannerText = 'Phaser + ES6 + Webpack'
+        let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
+        banner.font = 'Bangers'
+        banner.padding.set(10, 16)
+        banner.fontSize = 40
+        banner.fill = '#77BFA3'
+        banner.smoothed = false
+        banner.anchor.setTo(0.5)
 
-    this.mushroom = new Mushroom({
-      game: this,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
+        this.mushroom = new Mushroom({
+            game: this,
+            x: this.world.centerX,
+            y: this.world.centerY,
+            asset: 'mushroom',
+        });
 
-    this.game.add.existing(this.mushroom)
-  }
+        this.car = new Car({
+            game: this,
+            x: this.world.centerX,
+            y: this.world.centerY,
+            asset: 'car',
+        });
 
-  render () {
-    if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+        this.game.add.existing(this.mushroom);
+        this.game.add.existing(this.car);
     }
-  }
+
+    update () {
+
+    }
+
+    render () {
+        if (__DEV__) {
+            this.game.debug.spriteInfo(this.mushroom, 32, 32)
+        }
+    }
 }
