208e3be5400070d541ea470e5f2114312c817621
nicholas antonov <nicholasantonov@gmail.com>
2017-04-12 01:46:07 -0400

    minor tweaks

--- src/sprites/Car.js
+++ src/sprites/Car.js
@@ -1,33 +1,37 @@
 import Phaser from 'phaser'
+import { increase } from '../utils'
 
 export default class extends Phaser.Sprite {
     constructor ({ game, x, y, asset }) {
         super(game, x, y, asset)
         this.anchor.setTo(0.5)
         this.velocity = 0;
+        this.angularVelocity = 0;
         this.turningSpeed = 8;
     }
 
     update () {
-        const cursors = this.cursors;
-        console.log(cursors.up);
-        if (cursors.up.isDown && this.velocity <= 400) {
+        // forward/backward velocity
+        if (this.cursors.up.isDown && this.velocity <= 400) {
             this.velocity += 7;
-        } else if (cursors.down.isDown && this.velocity >= -400) {
+        } else if (this.cursors.down.isDown && this.velocity >= -400) {
             this.velocity -= 7;
-        } else if (this.velocity >= 7) {
-            this.velocity -= 7;
+        } else {
+            this.velocity = increase(this.velocity, -7);
         }
 
         this.body.velocity.x = this.velocity * Math.cos((this.angle-90)*0.01745);
         this.body.velocity.y = this.velocity * Math.sin((this.angle-90)*0.01745);
 
-        if (cursors.left.isDown)
+
+        // angular velocity
+        if (this.cursors.left.isDown) {
             this.body.angularVelocity = -1 * this.turningSpeed * (this.velocity/1000);
-        else if (cursors.right.isDown)
+        } else if (this.cursors.right.isDown) {
             this.body.angularVelocity = this.turningSpeed * (this.velocity/1000);
-        else
+        } else {
             this.body.angularVelocity = 0;
+        }
 
     }
 
--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,13 @@ export const centerGameObjects = (objects) => {
         object.anchor.setTo(0.5)
     })
 }
+
+export const increase = (number, amount) => {
+    const multiplier = number < 0 ? -1 : 1;
+    const ret = multiplier * (Math.abs(number) + amount);
+    // if the signs are different meaning we went past 0
+    if (ret * number <= 0) {
+        return 0;
+    }
+    return ret;
+}
