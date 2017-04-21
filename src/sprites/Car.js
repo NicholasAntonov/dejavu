8079eca5178c15773fc2b76810c4f499ed76e2e0
halfbro <mlagaren@stevens.edu>
2017-04-21 18:13:46 -0400

    Add drift

--- src/sprites/Car.js
+++ src/sprites/Car.js
@@ -6,32 +6,55 @@ export default class extends Phaser.Sprite {
     constructor ({ game, x, y, asset }) {
         super(game, x, y, asset)
         this.anchor.setTo(0.5)
-        this.velocity = 0;
+        this.targetvelocity = 0;
         this.angularVelocity = 0;
         this.turningSpeed = 8;
     }
 
     update () {
-        // forward/backward velocity
-        if (this.cursors.up.isDown && this.velocity <= 400) {
-            this.velocity += 7;
-        } else if (this.cursors.down.isDown && this.velocity >= -400) {
-            this.velocity -= 7;
+        var drifting = this.isDrifting();
+
+        if (this.cursors.up.isDown && this.targetvelocity <= 400) {
+            this.targetvelocity += 7;
+        } else if (this.cursors.down.isDown && this.targetvelocity >= -400) {
+            this.targetvelocity -= 7;
         } else {
-            this.velocity = increase(this.velocity, -7);
+            this.targetvelocity = increase(this.targetvelocity, -7);
         }
 
-        this.body.velocity.x = this.velocity * Math.cos((this.angle-90)*0.01745);
-        this.body.velocity.y = this.velocity * Math.sin((this.angle-90)*0.01745);
+        if (drifting) {
+            var xcomponent = Math.cos((this.angle-90) * 0.01745);
+            var ycomponent = Math.sin((this.angle-90) * 0.01745);
+            var anglevec = new Phaser.Point(xcomponent, ycomponent).setMagnitude(this.targetvelocity*1.2);
+            var currentvec = new Phaser.Point(this.body.velocity.x, this.body.velocity.y);
 
+            anglevec.subtract(currentvec.x, currentvec.y);
 
-        // angular velocity
-        if (this.cursors.left.isDown) {
-            this.body.angularVelocity = -1 * this.turningSpeed * (this.velocity/1000);
-        } else if (this.cursors.right.isDown) {
-            this.body.angularVelocity = this.turningSpeed * (this.velocity/1000);
+            this.body.velocity.x += anglevec.x*0.04;
+            this.body.velocity.y += anglevec.y*0.04;
+
+            if (this.cursors.left.isDown) {
+                this.body.angularVelocity = -1 * this.turningSpeed * (this.targetvelocity/1200);
+            } else if (this.cursors.right.isDown) {
+                this.body.angularVelocity = this.turningSpeed * (this.targetvelocity/1200);
+            } else {
+                this.body.angularVelocity = 0;
+            }
+
         } else {
-            this.body.angularVelocity = 0;
+            // forward/backward velocity
+            this.body.velocity.x = this.targetvelocity * Math.cos((this.angle-90)*0.01745);
+            this.body.velocity.y = this.targetvelocity * Math.sin((this.angle-90)*0.01745);
+
+
+            // angular velocity
+            if (this.cursors.left.isDown) {
+                this.body.angularVelocity = -1 * this.turningSpeed * (this.targetvelocity/3000);
+            } else if (this.cursors.right.isDown) {
+                this.body.angularVelocity = this.turningSpeed * (this.targetvelocity/3000);
+            } else {
+                this.body.angularVelocity = 0;
+            }
         }
 
         // Check if car is off the road
@@ -56,5 +79,21 @@ export default class extends Phaser.Sprite {
     initialize () {
         this.game.physics.p2.enable(this, false);
         this.cursors = game.input.keyboard.createCursorKeys();
+        this.body.damping = 0.6;
+    }
+
+    isDrifting () {
+        if (this.game.input.keyboard.isDown(Phaser.KeyCode.SHIFT)) return true;
+
+        var xcomponent = Math.cos((this.angle-90) * 0.01745);
+        var ycomponent = Math.sin((this.angle-90) * 0.01745);
+        var anglevec = new Phaser.Point(xcomponent, ycomponent);
+        var currentvec = new Phaser.Point(this.body.velocity.x, this.body.velocity.y);
+        if (currentvec.getMagnitude() < 100) return false;
+        currentvec.normalize();
+
+        var portion = anglevec.dot(currentvec);
+        if (portion < 0.9) return true;
+        else return false;
     }
 }
